7a3979e0cc60cd55ea9549fb975794468dcc517e
Ryan Laseter <proryanator@gmail.com>
2022-01-10 21:42:07 -0600

    Only show blood on wall or ground, not both

--- Assets/Scripts/GenerateSpriteMasksForGrid.cs
+++ Assets/Scripts/GenerateSpriteMasksForGrid.cs
@@ -12,10 +12,21 @@ using UnityEngine.Tilemaps;
 public class GenerateSpriteMasksForGrid : MonoBehaviour{
     
     private GameObject cellSpriteMaskPrefab;
+    // used to pull pre-configured sorting order data
+    private GameObject spriteSplatPrefab;
     private Tilemap[] tilemapsInScene;
-    
+
+    // data used to properly assign masks to a pre-set order
+    private int backgroundOrderInLayer;
+    private int foregroundOrderInLayer;
+
     private void Awake(){
         cellSpriteMaskPrefab = Resources.Load("PrefabSinglePlayer/Bloods/CellSpriteMask") as GameObject;
+        
+        spriteSplatPrefab = Resources.Load("PrefabSinglePlayer/Bloods/Splat Sprite") as GameObject;
+        Splat splat = spriteSplatPrefab.GetComponent<Splat>();
+        backgroundOrderInLayer = splat.orderLayerInBackGround;
+        foregroundOrderInLayer = splat.orderLayerInForeGround;
     }
     
     void Start(){
@@ -37,8 +48,7 @@ public class GenerateSpriteMasksForGrid : MonoBehaviour{
         }
     }
     
-    private void AddMaskSpritePerCell(Tilemap tilemap)
-    {
+    private void AddMaskSpritePerCell(Tilemap tilemap){
         foreach (Vector3Int tilePosition in tilemap.cellBounds.allPositionsWithin){
             if (!tilemap.HasTile(tilePosition)){
                 continue;
@@ -49,7 +59,16 @@ public class GenerateSpriteMasksForGrid : MonoBehaviour{
             
             // need to add half the grid cell size to match with the tile itself
             worldLocation += tilemap.cellSize / 2;
-            SpawnBloodSpriteMask(worldLocation, tile.sprite, tilemap.transform);
+            
+            // NOTE: this can all be more generalized, for now it's just on a foreground/background basis
+            string gridSortingLayer = tilemap.GetComponent<TilemapRenderer>().sortingLayerName;
+            
+            int sortingOrder = gridSortingLayer == "Background"
+                ? backgroundOrderInLayer
+                : foregroundOrderInLayer;
+
+            int sortingLayerId = tilemap.GetComponent<TilemapRenderer>().sortingLayerID;
+            SpawnBloodSpriteMask(worldLocation, tile.sprite, tilemap.transform, sortingOrder, sortingLayerId);
         }
     }
     
@@ -60,10 +79,19 @@ public class GenerateSpriteMasksForGrid : MonoBehaviour{
     /// <param name="location">Vector3 location to spawn the sprite mask</param>
     /// <param name="sprite">Sprite to set as the sprite mask</param>
     /// <param name="parent">The parent transform to hold all the sprite masks under</param>
-    private void SpawnBloodSpriteMask(Vector3 location, Sprite sprite, Transform parent){
+    /// <param name="sortingInLayer">The index from the splat prefab for this sprite masks' range</param>
+    private void SpawnBloodSpriteMask(Vector3 location, Sprite sprite, Transform parent, int sortingInLayer, int sortingLayerId){
         GameObject cellSpriteMask = Instantiate(cellSpriteMaskPrefab, location, Quaternion.identity);
         cellSpriteMask.transform.SetParent(parent);
         SpriteMask spriteMask = cellSpriteMask.GetComponent<SpriteMask>();
         spriteMask.sprite = sprite;
+        
+        // now set the range for the sprite mask to only affect a specific type of blood
+        // NOTE: must set front, then back (reverse will not set one of them)
+        spriteMask.frontSortingLayerID = sortingLayerId;
+        spriteMask.backSortingLayerID = sortingLayerId;
+
+        spriteMask.frontSortingOrder = sortingInLayer + 1;
+        spriteMask.backSortingOrder = sortingInLayer - 1;
     }
 }
